7fa480dd3aa71841c10dd7d76d4cc6c8a0622086
itsmeAB <ss.abhilash3@gmail.com>
2020-07-31 14:23:59 +0530

    add empty string instead of zero

--- src/components/Block/index.js
+++ src/components/Block/index.js
@@ -1,6 +1,14 @@
 import React from 'react';
+import getColour from '../../utils/getColours';
 import './Block.scss';
 
 export default function Block({ num }) {
-  return <div className='block' style={{color:num!==0 && '#645B52'}}>{num}</div>;
+  return (
+    <div
+      className='block'
+      style={{ background: getColour(num), color: num !== 0 && '#645B52' }}
+    >
+      {num ? num : ''}
+    </div>
+  );
 }
