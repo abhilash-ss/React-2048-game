cb502855c1683e44abade9cae672abd6711c96e8
itsmeAB <ss.abhilash3@gmail.com>
2020-08-14 11:18:31 +0530

    Updates

---
+++ precache-manifest.f00586c02baf31485a97d2625951d25c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "423e6e13549abe8422750302379ed47e",
+    "url": "/React-2048-game/index.html"
+  },
+  {
+    "revision": "e15f12996e61eaff756e",
+    "url": "/React-2048-game/static/css/main.3b66b2b5.chunk.css"
+  },
+  {
+    "revision": "125669fc86af8d1a8727",
+    "url": "/React-2048-game/static/js/2.7c99c562.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-2048-game/static/js/2.7c99c562.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e15f12996e61eaff756e",
+    "url": "/React-2048-game/static/js/main.942fbc91.chunk.js"
+  },
+  {
+    "revision": "15e4c0020c785f510667",
+    "url": "/React-2048-game/static/js/runtime-main.c5c9372a.js"
+  }
+]);
